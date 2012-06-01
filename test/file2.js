74df3c26823de91fdf75b00f060170f840e203a1
psq <psq@nanorails.com>
2012-05-31 20:35:50 -0700

    new underlying parsing strategy

---
+++ test/file2.js
@@ -0,0 +1,17 @@
+var
+  util = require("util"),
+  // assert = require('assert'),
+  // should = require('should'),
+  fs = require('fs'),
+  // jsp = require('../../psq-uglify-js/uglify-js').parser,
+  jsp = require('../../UglifyJS/uglify-js').parser,
+  jsdox = require("../lib/jsdox");
+
+
+fs.readFile('fixtures/test.js', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  var ast = jsp.parse(data.toString());
+  console.log(util.inspect(ast, false, 20, true));
+});
