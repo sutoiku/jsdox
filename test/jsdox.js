c7723c7b965ec22cd42d81f92e48bcfd58bf1b72
Vladimir de Turckheim <vlad2t@hotmail.com>
2014-09-04 08:37:15 +1000

    fix indent

--- test/jsdox.js
+++ test/jsdox.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec,
-  expect = require('expect.js'),
-  fs = require('fs');
+    expect = require('expect.js'),
+    fs = require('fs');
 
 var bin = 'bin/jsdox';
 
